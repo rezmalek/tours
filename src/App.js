e2ac59b8ccc9760c2de63f8a0951a33101c29d85
RezMalek <rezmalek7@gmail.com>
2021-04-05 16:26:13 -0700

    add styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import react, {useState, useEffect} from 'react';
+import react, { useState, useEffect } from 'react';
 import Loading from './Loading';
-import Tours from './Tours'
-import './App.css';
+import Tours from './Tours';
+import './index.css';
 
 // tours url
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  return <h1>Tours</h1>
+
+  return <h1>Tours</h1>;
 }
 
 export default App;
