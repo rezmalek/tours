be6da5489e1e297ad0b91e59326f0ed7b4131b4e
RezMalek <rezmalek7@gmail.com>
2021-04-06 10:07:59 -0700

    add read more and show less button

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 function Tour({ id, name, image, info, price, removeTour }) {
-   
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -10,7 +11,12 @@ function Tour({ id, name, image, info, price, removeTour }) {
           <h4>{name}</h4>
           <h4 className='tour-price'>{price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => setReadMore(!readMore) /* toggle true and false */}
+          >{readMore ? 'show less' : 'read more'}</button>
+        </p>
         <button className='delete-btn' onClick={() => removeTour(id)}>
           not interested
         </button>
